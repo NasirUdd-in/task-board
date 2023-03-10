bed58cfffbf130ce8cf7f1e6b01dff8c7f14fd96
MAC <mac@MACs-MacBook-Pro.local>
2023-03-10 12:17:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import "./styles.css";
 
-function App() {
+const initialTasks = {
+  doing: [
+    { id: 1, title: "Task 1" },
+    { id: 2, title: "Task 2" },
+    { id: 3, title: "Task 3" }
+  ],
+  done: [
+    { id: 4, title: "Task 4" },
+    { id: 5, title: "Task 5" }
+  ]
+};
+
+const App = () => {
+  const [tasks, setTasks] = useState(initialTasks);
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (!newTask) return;
+    const newId = Date.now();
+    const newTaskObj = { id: newId, title: newTask };
+    setTasks((prevTasks) => ({
+      ...prevTasks,
+      doing: [...prevTasks.doing, newTaskObj]
+    }));
+    setNewTask("");
+  };
+
+  const handleDeleteAll = () => {
+    setTasks((prevTasks) => ({ ...prevTasks, done: [] }));
+  };
+
+  const handleDragStart = (e, id) => {
+    e.dataTransfer.setData("taskId", id);
+  };
+
+  const handleDrop = (e, status) => {
+    const taskId = e.dataTransfer.getData("taskId");
+    const task = tasks.doing.find((task) => task.id === Number(taskId));
+    if (!task) return;
+    const newDoingTasks = tasks.doing.filter((task) => task.id !== Number(taskId));
+    const newDoneTasks = [...tasks.done, task];
+    setTasks({ doing: newDoingTasks, done: newDoneTasks });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h1 className="app__title">Kanban Board</h1>
+      <form className="app__form" onSubmit={handleAddTask}>
+        <input
+          className="app__input"
+          type="text"
+          value={newTask}
+          placeholder="Add new task"
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button className="app__button" type="submit">Add</button>
+      </form>
+      <div className="app__board">
+        <div className="app__column">
+          <h2 className="app__column-title">Doing</h2>
+          {tasks.doing.map((task) => (
+            <div
+              key={task.id}
+              className="app__task"
+              draggable
+              onDragStart={(e) => handleDragStart(e, task.id)}
+            >
+              {task.title}
+            </div>
+          ))}
+        </div>
+        <div
+          className="app__column"
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => handleDrop(e, "done")}
         >
-          Learn React
-        </a>
-      </header>
+          <h2 className="app__column-title">Done</h2>
+          {tasks.done.map((task) => (
+            <div key={task.id} className="app__task">
+              {task.title}
+            </div>
+          ))}
+          {tasks.done.length > 0 && (
+            <button className="app__delete-all" onClick={handleDeleteAll}>
+              Delete All
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
